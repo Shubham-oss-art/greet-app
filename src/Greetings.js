523c0ceadfabc5aff2056c86b7bc33ff9ef5c285
Shubham <shubhamsingh8199@gmail.com>
2021-08-30 23:04:49 +0530

    my first commit

---
+++ src/Greetings.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./index.css";
+
+let curDate = new Date(2020, 5, 5, 18);
+curDate = curDate.getHours();
+let greeting = "";
+
+const cssStyle = {};
+
+if (curDate >= 1 && curDate < 12) {
+  greeting = "Good Morning ";
+  cssStyle.color = "green";
+} else if (curDate >= 12 && curDate < 19) {
+  greeting = " Good Afternoon";
+  cssStyle.color = "Orange";
+} else {
+  greeting = "Good Night";
+  cssStyle.color = "brown";
+}
+
+function Greetings() {
+  return (
+    <div>
+      <h1>
+        Hello Sir,<span style={cssStyle}> {greeting} </span>
+      </h1>
+    </div>
+  );
+}
+
+export default Greetings;
